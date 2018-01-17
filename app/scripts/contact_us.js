a0eecab9961536337c4a7c90ef402e7b8079e9ac
1998MaxChykalo <maxymchykalo@gmail.com>
2018-01-17 18:45:31 +0200

    Some fixes to form

--- app/scripts/contact_us.js
+++ app/scripts/contact_us.js
@@ -1,14 +1,15 @@
 $(function() {
-    // $('.contact_us_container form').on('submit',function(event) {
-    //     event.preventDefault();
-    //     return false;
-    // });
     var errorMessages = {
         'required': 'This input field is required',
         'email': 'Please enter valid E-mail'
     }
+    $(window).on('unload',function(){
+        $('form').trigger('reset');
+    });
 
-    $('input').on('keyup',_.debounce(function(event) {
+    $('input').on('keyup',_.debounce(checkInput,500));
+
+    function checkInput(event) {
         if(!event.target.checkValidity()) {
             $(this).parent().siblings('hr').removeClass('valid').addClass('invalid');
             $(this).siblings('.errorMessage').show();
@@ -16,5 +17,5 @@ $(function() {
             $(this).parent().siblings('hr').removeClass('invalid').addClass('valid');
             $(this).siblings('.errorMessage').hide();
         }
-    },300));
+    }
 })
